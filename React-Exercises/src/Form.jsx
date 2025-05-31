e8ab9bd2ba89377237126edd9dd476f1dc4fb51c
fred00817 <mucyofred00@gmail.com>
2025-05-31 11:42:39 +0200

    added: Email,Name,Password even the form submits its self after SUBMISSION

--- React-Exercises/src/Form.jsx
+++ React-Exercises/src/Form.jsx
@@ -3,25 +3,50 @@ import React, { useState } from "react";
 function Formo() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleChange = (event) => {
     const updatedName = event.target.value;
     setName(updatedName);
+  };
+  const handleChang = (event) => {
+    const updatedEmail = event.target.value;
+    setEmail(updatedEmail);
+  };
 
-    const handleChang = (event) => {
-      const updatedEmail = event.target.value;
-      setEmail(updatedEmail);
-    };
+  const handleCha = (event) => {
+    const updatedPassword = event.target.value;
+    setPassword(updatedPassword);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+console.log("Form Submitted");
+console.log("Name", name);
+console.log("Email", email);
+console.log("Password", password);
+   
+    setName('')
+    setEmail('')
+   setPassword('')
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label> NAME : </label>
-      <input onChange={handleChange} />
-      <h1> hello!, {name}</h1>
+      <input type='text' value={name} onChange={handleChange} />
+      <h6> hello!, {name}</h6>
 
-      <label>Email :</label>
- <input value={email} onChange={handleChang} />
+      <label>EMAIL :</label>
+      <input type="email" value={email} onChange={handleChang} />
+      <h6>This is the my Email : {email}</h6>
+
+      <label>Password:</label>
+
+      <input type="password" value={password} onChange={handleCha} />
+
+      <h6>this my passWord: {password}</h6>
+      <button type="submit">SUBMIT</button>
     </form>
   );
 }
