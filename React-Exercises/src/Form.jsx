94473de19b6bbc17e64f95e0d31bd89981e59b77
fred00817 <mucyofred00@gmail.com>
2025-05-31 12:46:03 +0200

    added to show info you submitted

--- React-Exercises/src/Form.jsx
+++ React-Exercises/src/Form.jsx
@@ -4,7 +4,8 @@ function Formo() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [submittedUser, setSubmittedUser] = useState('')
+  
   const handleChange = (event) => {
     const updatedName = event.target.value;
     setName(updatedName);
@@ -24,14 +25,20 @@ function Formo() {
 console.log("Form Submitted");
 console.log("Name", name);
 console.log("Email", email);
-console.log("Password", password);
+    console.log("Password", password);
+    
+    setSubmittedUser({
+      name:name,
+      email: email
+    })
+    
    
-    setName('')
-    setEmail('')
-   setPassword('')
+  setName('')
+  setEmail('')
+  setPassword('')
   };
 
-  return (
+  return ( <div>
     <form onSubmit={handleSubmit}>
       <label> NAME : </label>
       <input type='text' value={name} onChange={handleChange} />
@@ -48,7 +55,19 @@ console.log("Password", password);
       <h6>this my passWord: {password}</h6>
       <button type="submit">SUBMIT</button>
     </form>
-  );
+
+  <div>
+ <h4>SUBMITTED INFORMATION</h4>
+  <h6><strong>NAME:</strong>{submittedUser.name}</h6>
+    <h6><strong>Email:</strong>{submittedUser.email}</h6>
+    </div>
+  </div>
+    
+    
+  )
+
 }
+
+
 
 export default Formo;
